c38fd9c65e341556b67c98b248f482bb2b84e1e7
Tilman <tilman@tilman.de>
2013-10-18 11:55:27 +0200

    even more DAOs

--- nodebucket-main/src/main/java/de/tilman/nodebucket/Node.java
+++ nodebucket-main/src/main/java/de/tilman/nodebucket/Node.java
@@ -7,13 +7,33 @@ public abstract class Node {
 
 	public enum Visibility { PUBLIC, PRIVATE, GROUP };
 	
+	public enum MimeType {
+		TEXT ("application/xhtml+xml"),
+		FILE ("application/octet-stream"),
+		JPEG ("image/jpeg");
+		
+		private final String mimeType;
+		
+		private MimeType(String mimeType) {
+			this.mimeType = mimeType;
+		}
+		
+		public String getType() {
+			return mimeType;
+		}
+	}
+	
 	private String title;
-	private byte[] content;
-	private String mimeType;
+	private MimeType mimeType;
 	private Visibility visibility;
 	private Set<String> tags = new HashSet<String>();
 	private Set<Node> backlinks = new HashSet<Node>();
 	
+	
+	public Node(String title, MimeType mimeType) {
+		this.title = title; // TODO check if Node with this title already exists
+		this.mimeType = mimeType;
+	}
 
 	public String getTitle() {
 		return title;
@@ -23,20 +43,8 @@ public abstract class Node {
 		this.title = title;
 	}
 
-	public String getMimeType() {
+	public MimeType getMimeType() {
 		return mimeType;
-	}
-
-	public void setMimeType(String mimeType) {
-		this.mimeType = mimeType;
-	}
-
-	public byte[] getContent() {
-		return content;
-	}
-
-	public void setContent(byte[] content) {
-		this.content = content;
 	}
 
 	public Visibility getVisibility() {
@@ -46,5 +54,25 @@ public abstract class Node {
 	public void setVisibility(Visibility visibility) {
 		this.visibility = visibility;
 	}
+	
+	public void addTag(String tag) {
+		tags.add(tag);
+	}
+	
+	public void removeTag(String tag) {
+		tags.remove(tag);
+	}
+	
+	public void addBacklink(Node node) {
+		backlinks.add(node);
+	}
+	
+	public void removeBacklink(Node node) {
+		backlinks.remove(node);
+	}
+	
+	public abstract Object getContent();
+
+	public abstract void setContent(byte[] content);
 
 }
